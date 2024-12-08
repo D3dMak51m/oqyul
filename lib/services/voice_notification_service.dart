cdf5010123634d4d579c7db0baebeb3691929c2a
D3dMak51m <o.fazliddinov.f@gmail.com>
2024-12-08 14:36:56 +0500

    optimized for budget smartphones: implemented k-tree and tiles for google maps

---
+++ lib/services/voice_notification_service.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:audioplayers/audioplayers.dart';
+
+class VoiceNotificationService {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  String currentLanguage = 'rus';
+
+  VoiceNotificationService();
+
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+
+  Future<void> playVoiceNotification(int cameraType, int distance) async {
+    // Остановка предыдущего воспроизведения
+    await _audioPlayer.stop();
+
+    String typeStr = cameraType.toString();
+    final file1 = await _getSoundFile(currentLanguage, typeStr, '1');
+    final fileDist = await _getSoundFile(currentLanguage, typeStr, distance.toString());
+
+    await _playSequenceOfSounds([file1, fileDist]);
+  }
+
+  Future<String> _getSoundFile(String lang, String type, String fileName) async {
+    // Путь: assets/sounds/<lang>/<type>/<fileName>.mp3
+    String path = 'assets/sounds/$type/$fileName.mp3';
+    if (!await _assetExists(path)) {
+      String defaultPath = 'assets/sounds/$fileName.mp3';
+      if (await _assetExists(defaultPath)) {
+        return defaultPath;
+      } else {
+        print("Sound file not found: $path and no default");
+        return '';
+      }
+    }
+    return path;
+  }
+
+  Future<bool> _assetExists(String path) async {
+    try {
+      await rootBundle.load(path);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> _playSequenceOfSounds(List<String> files) async {
+    for (var file in files) {
+      if (file.isEmpty) continue;
+      await _audioPlayer.play(AssetSource(file.replaceFirst('assets/', '')), volume: 1.0);
+      await _waitForComplete();
+    }
+  }
+
+  Future<void> _waitForComplete() async {
+    Completer<void> completer = Completer();
+    StreamSubscription? sub;
+    sub = _audioPlayer.onPlayerComplete.listen((event) {
+      sub?.cancel();
+      completer.complete();
+    });
+    await completer.future;
+  }
+}
